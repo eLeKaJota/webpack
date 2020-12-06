2b8ec26a728a1eeaf35529ddee9b99998d4f5aca
Zifu <compraunzifupornavidad@gmail.com>
2020-12-06 19:08:13 +0100

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { saludar } from './js/componentes';
+import './styles.css';
+
+const nombre = 'Caras de culo';
+
+saludar(nombre);
---
+++ src/js/componentes.js
@@ -0,0 +1,8 @@
+import '../CSS/componentes.css'
+
+export const saludar = (nombre) =>{
+    console.log('Creando etiqueta h1');
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola, ${nombre}, Cara de moco!`;
+    document.body.append(h1);
+}
